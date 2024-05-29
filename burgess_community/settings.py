60ca22be669a479baf41d1a8c0c89953970f732b
Cam Wheatley <cameron_2000@live.co.uk>
2024-05-29 21:09:11 +0000

    connect sql database to project, create superuser

--- burgess_community/settings.py
+++ burgess_community/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&s!or$g%2-_g@l6h7r_sv)meskmi(mn(586a4^xgatkg2o^a6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-cameronjame-burgesscomm-0jpc33qk5v0.ws-us114.gitpod.io',
                 '.herokuapp.com',]
@@ -75,11 +79,15 @@ WSGI_APPLICATION = 'burgess_community.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
