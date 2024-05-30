513d5daffb4bcc8d14f8ffad9c58fcd5260c3b8e
Cam Wheatley <cameron_2000@live.co.uk>
2024-05-30 13:07:49 +0000

    install summernote, wire it up to discussion model

--- discussion/admin.py
+++ discussion/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Discussion, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 
-admin.site.register(Discussion)
+@admin.register(Discussion)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 admin.site.register(Comment)
