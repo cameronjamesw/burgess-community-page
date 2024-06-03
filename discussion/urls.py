3187bd0a3ea0b4d2e004e7fb27ae9eb4725224a2
Cam Wheatley <cameron_2000@live.co.uk>
2024-06-03 21:15:31 +0000

    create discussion_content html, create discussion_content view

--- discussion/urls.py
+++ discussion/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.DiscussionList.as_view(), name='home'),
+    path('<slug:slug>/', views.discussion_content, name='discussion_content'),
 ]
--- discussion/views.py
+++ discussion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Discussion
 
@@ -7,3 +7,13 @@ from .models import Discussion
 class DiscussionList(generic.ListView):
     queryset = Discussion.objects.all()
     template_name = "discussion/index.html"
+
+def discussion_content(request, slug):
+    queryset = Discussion.objects.filter(status=1)
+    discussion = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "discussion/discussion_content.html",
+        {"discussion": discussion},
+    )
