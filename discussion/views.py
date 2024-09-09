a38d7ddf505f656901999fca90ab3cf69d6c9458
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-09 19:24:06 +0000

    wire up comment post request

--- discussion/views.py
+++ discussion/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Discussion
 from .forms import CommentForm
 
@@ -14,6 +15,19 @@ def discussion_content(request, slug):
     discussion = get_object_or_404(queryset, slug=slug)
     comments = discussion.comments.all().order_by("-created_on")
     comment_count = discussion.comments.filter(approved=True).count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.discussion = discussion
+            comment.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Comment successfully submitted, now awaiting approval'
+        )
+
     comment_form = CommentForm()
 
     return render(
