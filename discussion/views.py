fd728b569afac7028f315958f9e7eb310d83afb6
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-09 18:48:17 +0000

    add crispy forms, add comment form to discussion page, style form

--- discussion/views.py
+++ discussion/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Discussion
+from .forms import CommentForm
 
 # Create your views here.
 
@@ -13,11 +14,14 @@ def discussion_content(request, slug):
     discussion = get_object_or_404(queryset, slug=slug)
     comments = discussion.comments.all().order_by("-created_on")
     comment_count = discussion.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,
         "discussion/discussion_content.html",
         {"discussion": discussion,
         "comments": comments,
-        "comment_count": comment_count},
+        "comment_count": comment_count,
+        "comment_form": comment_form},
+        
     )
