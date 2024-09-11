e20e33287903f4e33f622f05d343094a9a8731ba
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-11 18:14:59 +0000

    add discussion modal, wire up button with js

---
+++ static/js/discussion.js
@@ -0,0 +1,9 @@
+const discussionBtn = document.getElementById("createDiscussionButton");
+
+const discussionModal = new bootstrap.Modal(document.getElementById("discussionModal"));
+
+discussionBtn.addEventListener("click", (e) => {
+    discussionModal.show();
+    }
+);
+
