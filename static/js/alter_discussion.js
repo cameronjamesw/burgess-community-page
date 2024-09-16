1fd849af274caf006b50bcf4a7ce0d84a980d4ca
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-16 23:19:37 +0000

    add comments to alter_discussion.js

--- static/js/alter_discussion.js
+++ static/js/alter_discussion.js
@@ -1,12 +1,21 @@
+/** These variables refer to the selected buttons */
+
 const editDiscussionBtn = document.getElementById("editDiscussionBtn");
+const submitBtn = document.getElementById("submitBtn");
+
+/** These variables refer to the selected text fields */
 const discussionText = document.getElementById("id_content");
 const discussionTitleField = document.getElementById("id_title");
 const discussionTitle = document.getElementById("discussionTitle");
 const discussionContent = document.getElementById("discussionContent");
-const submitBtn = document.getElementById("submitBtn");
 
+/** These variables refer to the modals  */
 const editDiscussionModal = new bootstrap.Modal(document.getElementById("editDiscussionModal"));
 
+/** This event listener shows the Edit Discussion Modal
+ * as well as changing the values of the form fields to
+ * the current values of the discussion.
+ */
 editDiscussionBtn.addEventListener("click", (e) => {
     let discussionContentValue = discussionContent.innerText;
     discussionText.value = discussionContentValue;
