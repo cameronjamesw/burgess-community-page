233bf29e2334fa03feaa04e1442b34ae075341f6
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-16 18:19:07 +0000

    begin to wire up editDiscussion form

--- static/js/alter_discussion.js
+++ static/js/alter_discussion.js
@@ -1,7 +1,17 @@
 const editDiscussionBtn = document.getElementById("editDiscussionBtn");
+const discussionText = document.getElementById("id_content");
+const discussionTitleField = document.getElementById("id_title");
+const discussionTitle = document.getElementById("discussionTitle");
+const discussionContent = document.getElementById("discussionContent");
 
 const editDiscussionModal = new bootstrap.Modal(document.getElementById("editDiscussionModal"));
 
 editDiscussionBtn.addEventListener("click", (e) => {
+    let discussionContentValue = discussionContent.innerText;
+    discussionText.value = discussionContentValue;
+
+    let discussionTitleValue = discussionTitle.innerText;
+    discussionTitleField.value = discussionTitleValue;
+
     editDiscussionModal.show();
 });
