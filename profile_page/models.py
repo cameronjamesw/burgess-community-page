aced5139f12a4eed27646bb67c000a321e6ee999
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-17 21:47:24 +0000

    create user_profile model, add to admin.py

--- profile_page/models.py
+++ profile_page/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
+CAMPS = ((0, "Burgess"), (1, "Hayward"))
 # Create your models here.
+
+class User_Profile(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )
+    camp = models.IntegerField(choices=CAMPS, default=0)
+    position = models.CharField(max_length=50)
+    tenure = models.IntegerField(default=0)
+    facts = models.TextField(blank=False)
+    memory = models.TextField(blank=False)
