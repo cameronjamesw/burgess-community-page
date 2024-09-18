04f92f1f38935034223b47e79d8a721372b15277
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-18 18:46:01 +0000

    create user_profile.html, link to viewd.py

--- profile_page/views.py
+++ profile_page/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from .models import User_Profile
 from django.http import HttpResponse
 
 # Create your views here.
 
 def display_profile(request):
-    return HttpResponse("This is the profile page!!")
+    user = request.user
+    queryset = User_Profile.objects.all()
+    profile = get_object_or_404(queryset)
+
+    context = {
+        "profile": profile
+    }
+
+    return render(
+        request,
+        "profile/user_profile.html",
+        context
+    )
