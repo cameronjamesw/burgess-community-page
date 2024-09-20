4b1e7113ef9024279ffc0497a0da2175b61abf69
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-20 15:52:29 +0000

    add photo field to staff model, add sample profiles

--- staff/models.py
+++ staff/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 CAMPS = ((0, "Burgess"), (1, "Hayward"))
 # Create your models here.
@@ -8,6 +9,7 @@ class Staff_Member(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE
     )
+    photo = CloudinaryField("image", default="placeholder")
     camp = models.IntegerField(choices=CAMPS, default=0)
     position = models.CharField(max_length=50)
     tenure = models.IntegerField(default=0)
