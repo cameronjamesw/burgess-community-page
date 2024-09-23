377c5676ed52a1370982511bdeaef07f7ac331e4
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-23 16:33:47 +0000

    add comments to discussion/forms.py

--- discussion/forms.py
+++ discussion/forms.py
@@ -1,11 +1,14 @@
 from .models import Discussion, Comment
 from django import forms
 
+# This is the Discussion Form where users can create a discussion 
 class DiscussionForm(forms.ModelForm):
     class Meta:
         model = Discussion
         fields = ('title', 'excerpt', 'content')
         
+
+# This is the Comment Form where users can create a comment 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
