e5cb0b7229985803580e153f66261422189d1f34
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-23 16:48:55 +0000

    add comments to staff/models/py

--- staff/models.py
+++ staff/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+# This refers to which camp the user works at
 CAMPS = ((0, "Burgess"), (1, "Hayward"))
+
 # Create your models here.
 
+# This model refers to the Staff Members
 class Staff_Member(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE
     )
     photo = CloudinaryField("image", default="placeholder")
+
+    # Camp refers to which camp they are working at, Burgess or Hayward
     camp = models.IntegerField(choices=CAMPS, default=0)
     position = models.CharField(max_length=50)
+
+    # Tenure refers to how longer they have been employed by the camp
     tenure = models.IntegerField(default=0)
     facts = models.TextField(blank=False)
     memory = models.TextField(blank=False)
