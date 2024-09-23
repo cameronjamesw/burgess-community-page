90e7c50427c730d5dcec4f16b501c8751614bddc
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-23 16:46:34 +0000

    add comments to discussion/views.py

--- discussion/views.py
+++ discussion/views.py
@@ -8,14 +8,18 @@ from .forms import DiscussionForm, CommentForm
 
 # Create your views here.
 
+# This is the discussion list view
 def discussion_list(request):
     discussions = Discussion.objects.filter(status=1, approved=True).order_by("-created_on")
    
+    # This if statement refers to the Discussion Form
     if request.method == "POST":
         discussion_form = DiscussionForm(data=request.POST)
         if discussion_form.is_valid():
             discussion = discussion_form.save(commit=False)
             discussion.author = request.user
+
+            # Slugify creates a slug based off of the title field 
             discussion.slug = slugify(discussion.title)
             discussion.save()
             messages.add_message(
@@ -36,12 +40,16 @@ def discussion_list(request):
         context,
     )
 
+# This is the discussion content view
 def discussion_content(request, slug):
     queryset = Discussion.objects.filter(status=1)
     discussion = get_object_or_404(queryset, slug=slug)
+
+    # Comments are ordered by date created
     comments = discussion.comments.all().order_by("-created_on")
     comment_count = discussion.comments.filter(approved=True).count()
 
+    # This if statement refers to the comment form
     if request.method == "POST":
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -68,17 +76,21 @@ def discussion_content(request, slug):
         },
     )
 
+# This is the comment edit view 
 def comment_edit(request, slug, comment_id):
     """
     view to edit comments
     """
-    if request.method == "POST":
 
+    # This if statement refers to editing comment 
+    if request.method == "POST":
         queryset = Discussion.objects.filter(status=1)
         discussion = get_object_or_404(queryset, slug=slug)
         comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
 
+        # This if statement ensures users can only edit their 
+        # own comments, otherwise throws an error 
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
             comment.discussion = discussion
@@ -88,8 +100,10 @@ def comment_edit(request, slug, comment_id):
         else:
             messages.add_message(request, messages.ERROR, 'Error updating comment!')
 
+    # Upon submission, user is returned to the initial discussion
     return HttpResponseRedirect(reverse('discussion_content', args=[slug]))
 
+# This view refers to deleting a comment
 def comment_delete(request, slug, comment_id):
     """
     view to delete comment
@@ -98,24 +112,32 @@ def comment_delete(request, slug, comment_id):
     discussion = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
 
+    # This if statement ensures users can only delete
+    # their own comments
     if comment.author == request.user:
         comment.delete()
         messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
     else:
         messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
 
+    # The user is returned to the initial discussion 
     return HttpResponseRedirect(reverse('discussion_content', args=[slug]))
 
+# This view refers to editing a discussion
 def discussion_edit(request, slug):
     """
     This is the view to edit an exisitng discussion.
     """
 
+    # This if statement ensures the method is correct for the
+    # edit discussion form 
     if request.method == "POST":
         queryset = Discussion.objects.all()
         discussion = get_object_or_404(queryset, slug=slug)
         discussion_forn = DiscussionForm(data=request.POST, instance=discussion)
 
+        # This if statement ensures only the author can
+        # edit their own discussions
         if discussion_forn.is_valid() and discussion.author == request.user:
             discussion = discussion_forn.save(commit=False)
             discussion.approved = False
@@ -124,16 +146,22 @@ def discussion_edit(request, slug):
         else:
             messages.add_message(request, messages.ERROR, 'Error updating discussion!')
 
+        # The user will be returned to the home page
         return HttpResponseRedirect(reverse('home'))
 
+# This view refers to deleting a discussion
 def discussion_delete(request, slug):
     queryset = Discussion.objects.all()
     discussion = get_object_or_404(queryset, slug=slug)
 
+    # This if statement ensures only the author of the
+    # discussion is able to delete it
     if discussion.author == request.user:
         discussion.delete()
         messages.add_message(request, messages.SUCCESS, 'Discussion successfully deleted!')
     else:
         messages.add_message(request, messages.ERROR, 'You can only delete your own discussions!')
 
+    # This return statement sends the user to the home page
+    # as the discussion no longer exsists
     return HttpResponseRedirect(reverse('home'))
