fe417a31aa62a5547e688ef354e29e897503d105
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-23 20:16:01 +0000

    add staff profile form to staff/views.py

--- staff/views.py
+++ staff/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from .models import Staff_Member
 from django.http import HttpResponse
+from django.contrib import messages
+from .models import Staff_Member
+from .forms import ProfileForm
 
 # Create your views here.
 
@@ -8,9 +10,32 @@ from django.http import HttpResponse
 def display_staff(request):
     staff_profiles = Staff_Member.objects.all()
 
+# This if statement is regarding the Staff Profile Form 
+    if request.method == 'POST':
+        profile_form = ProfileForm(data=request.post)
+        if profile_form.is_valid():
+            staff_profile = profile_form.save(commit=False)
+            staff_profile.user = request.user
+            staff_profile.save()
+            messages.add_message(
+                request, messgaes.SUCCESS,
+                "Staff profile successfully created!"
+            )
+        else:
+            messages.add_message(
+                request, ERROR,
+                "Error creating staff profile, please try again."
+            )
+
+
+    profile_form = ProfileForm()
+
     context = {
-        "staff_profiles": staff_profiles
+        "staff_profiles": staff_profiles,
+        "profile_form": profile_form
     }
+
+    
 
     return render(
         request,
