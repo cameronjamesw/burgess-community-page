239f32710ec5b67c517d22bd501e115281aff872
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-23 16:54:56 +0000

    add comment to discussion.js

--- static/js/discussion.js
+++ static/js/discussion.js
@@ -1,7 +1,10 @@
 const discussionBtn = document.getElementById("createDiscussionButton");
-
 const discussionModal = new bootstrap.Modal(document.getElementById("discussionModal"));
 
+/**
+ * This event listener shows the Create Discussion Modal
+ * when the 'Create Discussion' button is clicked by the user
+ */
 discussionBtn.addEventListener("click", (e) => {
     discussionModal.show();
     }
