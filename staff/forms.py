b8a99a8671224c2c78f6afc5c80161caf2d6a80b
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-25 19:10:12 +0000

    fix validation errors in staff/forms.py

--- staff/forms.py
+++ staff/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import Staff_Member
 
+
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Staff_Member
-        fields = ('photo', 'camp', 'position', 'tenure', 'facts', 'memory' )
+        fields = ('photo', 'camp', 'position', 'tenure', 'facts', 'memory')
         labels = {
             'photo': 'Profile Photo',
             'tenure': 'Years Worked',
             'facts': 'Interesting Fact',
             'memory': 'Favourite Memory'
         }
-
