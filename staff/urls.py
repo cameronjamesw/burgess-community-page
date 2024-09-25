fd097072dcaafda966a5854857d3cb7548b97822
Cam Wheatley <cameron_2000@live.co.uk>
2024-09-25 19:08:00 +0000

    fix validation errors in staff/urls.py

--- staff/urls.py
+++ staff/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('staff_page/', views.display_staff, name='staff_page'),
-    path('staff_page/burgess/', views.display_burgess_staff, name='burgess_staff'),
-    path('staff_page/hayward/', views.display_hayward_staff, name='hayward_staff'),
+    path('staff_page/', views.display_staff,
+         name='staff_page'),
+    path('staff_page/burgess/', views.display_burgess_staff,
+         name='burgess_staff'),
+    path('staff_page/hayward/', views.display_hayward_staff,
+         name='hayward_staff'),
 ]
