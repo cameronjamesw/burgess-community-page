2dff93369e43e03d7d75d51aa04828a53c8f3217
Cameron Wheatley <cameron_2000@live.co.uk>
2025-01-16 13:49:05 +0000

    fix aria-hidden error in staff.js

--- static/js/staff.js
+++ static/js/staff.js
@@ -1,6 +1,20 @@
 const createProfileBtn = document.getElementById("createProfileBtn");
 const profileModal = new bootstrap.Modal(document.getElementById("createProfileModal"));
 
+/**
+ * This code overrides standard Bootstrap which was causing
+ * issues between modals and screenreaders. This code allows
+ * screenreaders to recognise that the modal is hidden and when
+ * it is active.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('hide.bs.modal', function (event) {
+        if (document.activeElement) {
+            document.activeElement.blur();
+        }
+    });
+});
+
 createProfileBtn.addEventListener('click', (e => {
     profileModal.show();
 }));
