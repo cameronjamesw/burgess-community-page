43f02d5bf1383080829505fee0b2242fa36f5d7a
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-14 14:25:23 -0400

    update settings.py

--- burgess_community/settings.py
+++ burgess_community/settings.py
@@ -30,9 +30,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-cameronjame-burgesscomm-pz47xl890y2.ws-us116.gitpod.io',
-                '8000-cameronjame-burgesscomm-2qessdbtfhv.ws.codeinstitute-ide.net',
-                '.herokuapp.com',]
+ALLOWED_HOSTS = [
+                '.herokuapp.com',
+                '127.0.0.1',
+                '127.0.0.1:8000',
+                ]
 
 
 # Application definition
@@ -95,17 +97,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'burgess_community.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
