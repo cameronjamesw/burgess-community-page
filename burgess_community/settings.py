4bd0f975601af5e42913f032393eb71b34b7fcac
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-14 16:03:16 -0400

    update allowed hosts for Render

--- burgess_community/settings.py
+++ burgess_community/settings.py
@@ -31,10 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = [
-                '.herokuapp.com',
                 '127.0.0.1',
                 '127.0.0.1:8000',
-                'burgess-community-page-57f369ca8521.herokuapp.com/'
+                'https://burgess-community-page.onrender.com/'
                 ]
 
 
@@ -104,7 +103,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://burgess-community-page-57f369ca8521.herokuapp.com/',
+    'https://burgess-community-page.onrender.com/',
 ]
 
 
